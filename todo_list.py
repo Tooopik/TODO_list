89520b5403187f45ea2128050dbaa227d99daeda
Adrian Wójcik <adrian.wojcik00@gmail.com>
2022-05-18 11:02:55 +0200

    Add edit task name and task status in edit method

--- todo_list.py
+++ todo_list.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from datetime import datetime
 import pickle
 
@@ -13,12 +14,59 @@ class Task:
         self.status = taskStatus
         self.taskList.append(self)
 
-    # def addTask(self):
-     #   self.task = 'test'
-     #   self.deadLine = datetime(input('Deadline (y,m,d) >>> '))
-     #   self.addDate = datetime.now()
-     #   self.status = self.taskStatus[0]
-     #   self.taskList.append({self.task: [self.status, self.addDate, self.deadLine]})
+    @staticmethod
+    def editTask():
+        print('Enter task ID to edit:')
+        taskId = int(input('>>> '))
+        print(
+            f'\nTask: {Task.taskList[taskId-1].task} | Status: {Task.taskList[taskId-1].status} | Deadline: {Task.taskList[taskId-1].deadLine}\n')
+
+        if taskId <= len(Task.taskList):
+            print('What do you want to edit?')
+            print('1 - name\n2 - status\n3 - deadline')
+            userChoice = int(input('>>> '))
+            if userChoice in [1, 2, 3]:
+                if userChoice == 1:
+                    print(f'Current name: {Task.taskList[taskId-1].task}')
+                    print('Enter new name:')
+                    newName = input('>>> ')
+                    if len(newName) >= 1:
+                        Task.taskList[taskId-1].task = newName
+                        print(
+                            f'Task name changed to: {Task.taskList[taskId-1].task}\n')
+                    else:
+                        print(f'New name "{newName}" is not correct')
+
+                elif userChoice == 2:
+                    print(f'Current status: {Task.taskList[taskId-1].status}')
+                    print('Select status:')
+                    for idx, status in enumerate(Task.taskStatus, start=1):
+                        print(f'{idx} - {status}')
+                    userChoice = int(input('>>> '))
+                    if userChoice in range(0, len(Task.taskStatus)+1):
+                        Task.taskList[taskId -
+                                      1].status = Task.taskStatus[userChoice-1]
+                        print(
+                            f'Status changed to: {Task.taskList[taskId -1].status}\n')
+                    else:
+                        print(f'Number {userChoice} is wrong')
+
+                elif userChoice == 3:
+                    pass
+            else:
+                print('Use number from range')
+        else:
+            print(f'Task with ID {taskid} not exist')
+
+    @staticmethod
+    def deleteTask():
+        print('Enter task ID to delete:')
+        taskId = int(input('>>> '))
+        try:
+            del Task.taskList[taskId - 1]
+            print('Task deleted\n')
+        except:
+            print('error')
 
     @ staticmethod
     def numberOfTask():
@@ -49,14 +97,14 @@ class Task:
 def menu():
     while True:
         print('what do you want to do?')
-        print('1 - show all TODO postions\n2 - Add new TODO position\n3 - delete position\n4 - Exit')
+        print('1 - show all TODO postions\n2 - Add new TODO position\n3 - edit position\n4 - delete position\n5 - Exit')
         try:
             userChoice = int(input('>>> '))
         except ValueError:
             userChoice = 0
             print('Error - select from numbers!')
 
-        if userChoice in [1, 2, 3, 4]:
+        if userChoice in [1, 2, 3, 4, 5]:
             break
     return userChoice
 
@@ -80,9 +128,12 @@ while True:
         print('New task added')
 
     elif userChoice == 3:
-        print('Possition deleted')
+        Task.editTask()
 
     elif userChoice == 4:
+        Task.deleteTask()
+
+    elif userChoice == 5:
         break
 
 Task.saveTasklist()
